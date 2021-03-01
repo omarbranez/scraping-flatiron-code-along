1341a08d424e7c3a54d9bc72c69bede2abf28f88
Omar Branez <omar.branez@gmail.com>
2021-03-01 00:48:48 -0500

    commit tree b5326059286545b2e8603314a6c22a1a511458a5

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -15,8 +16,33 @@ class Scraper
       end
     end
   end
+
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")) #the page
+    #binding.pry
+    # doc.css(".post").each do |post| #each offering had a class of post, so we iterate on it
+    #     course = Course.new #create instances
+    #     course.title = post.css("h2").text # we analyzed each element of nokogiri's array
+    #     course.schedule = post.css(".date").text
+    #     course.description = post.css("p").text
+    # end
+    #### all the above was refactored into two other methods, get_courses and make_courses
+  end
   
+  def get_courses
+    self.get_page.css(".post") #use each preceding function inside the new one
+  end
+
+  def make_courses
+    self.get_courses.each do |post| #use each preceding function inside the new one
+        course = Course.new #create instances
+        course.title = post.css("h2").text # we analyzed each element of nokogiri's array
+        course.schedule = post.css(".date").text
+        course.description = post.css("p").text
+    end
+  end
+
 end
 
-
+Scraper.new.print_courses
 
